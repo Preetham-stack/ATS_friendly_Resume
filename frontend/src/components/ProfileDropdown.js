0f3ab18d4ae91fe0b2c40aee255f5fa6d4f9eec7
Preetham-stack <preetham.paladi@vation.com>
2025-10-27 16:22:45 +0530

    Updated the Header with profile and back buttons.

---
+++ frontend/src/components/ProfileDropdown.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ProfileDropdown.css';
+
+const ProfileDropdown = () => {
+  return (
+    <div className="profile-dropdown">
+      <div className="user-info">
+        <p className="user-name">preetham</p>
+        <p className="user-designation">java consultant</p>
+        <p className="user-email">preetham.paladi@vation.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileDropdown;
